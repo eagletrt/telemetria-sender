e0abdc33d3492cfac39685b5966d128dc7485a7b
Iron512 <ivanmartini96@gmail.com>
2018-11-07 17:33:08 +0100

    description fix + minor fixes

--- docs/config/can_manual.sh
+++ docs/config/can_manual.sh
@@ -13,12 +13,12 @@
 * Utilizzo dei programmini base per CAN BUS *
 *********************************************
 
-Cartella di lavoro: examples
+Cartella di lavoro: examples/basic
 Files: sender.c e receiver.c
 
 Compilare
-	$ gcc sender.c -o sender
-	$ gcc receiver.c -o receiver
+	$ gcc sender.c plugin/can_custom_lib.c -o sender
+	$ gcc receiver.c plugin/can_custom_lib.c -o receiver
 
 Aprire un terminale ed eseguire receiver
 	$ ./receiver
@@ -27,5 +27,19 @@ Aprire un altro terminale ed eseguire sender
 	$ ./sender
 
 In receiver vedremo i dati mandati dal sender (in questo caso dati predefiniti)
-Adesso sto creando delle librerie per agevolarci l'utilizzo del can
 
+*********************************************
+
+Cartella di lavoro: examples/token
+Files: token.c
+
+Compilare
+	$ gcc token.c plugin/can_custom_lib.c -o token
+
+Aprire diversi terminali e eseguire più istanze di token
+	$./token
+
+Ogni istanza avrà un identificativo diverso. 
+le verie istanze possono comunicare tra di loro (con un sistema a token)
+Il proprietario del token può mandare un messaggio (a un istanza o in broadcast) o inviare il token.
+Chi non possiede il token può solo ricevere i messaggi.
