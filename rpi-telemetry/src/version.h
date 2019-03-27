8260a754c0f79ef74d14b056b557e6d9cb2f118a
iron512 <ivanmartini96@gmail.com>
2019-03-27 13:43:29 +0100

    minor fix

--- rpi-telemetry/src/version.h
+++ rpi-telemetry/src/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define GIT_VERSION_TAG "0.1.0"
-#define GIT_COMMIT_HASH "0.1.0-66-gfb906cb§"
-#define CMAKE_BUILD_TYPE "Debug"
+#define GIT_VERSION_TAG ""
+#define GIT_COMMIT_HASH ""
+#define CMAKE_BUILD_TYPE ""
 
 #endif
