98fa5023e7290278e9624c008ad338696d9a0b90
Nicola Toscan <nicolatoscan99@outlook.it>
2020-11-06 05:26:44 +0000

    test: added checks on mongo_start_session tests

--- test/partial/source/souites/mongo_services/mongo_services.test.ts
+++ test/partial/source/souites/mongo_services/mongo_services.test.ts
@@ -1,18 +1,50 @@
+import { clean } from 'mongo-cleaner';
 import * as path from 'path';
-import { runTests } from "../../utils/misc"
+import { runTests, getMongoUri } from "../../utils/misc"
+import { MongoClient } from 'mongodb';
+import * as assert from 'assert';
 
-export default async function() {
-    describe('mongo_services', function() {
-        runTests(
-            path.join(__dirname, 'mongo_start_session.test.out'),
-            [{
-                testDescription: 'Testone',
-                args: [ 'pilotone', 'circuitone', 'localhost', '27017', 'eagle_test', 'chimera' ],
+const MONGO_CONFIG = {
+    host: 'localhost',
+    port: 27017,
+    db: 'eagle_test'
+}
 
-            }],
-            async (prop) => {
-                //console.log(prop.result);
-            }
-        );
+export default async function () {
+    describe('mongo_services', function () {
+
+        describe('mongo_start_session', function () {
+
+            beforeEach(async function () {
+                await clean(getMongoUri(MONGO_CONFIG.host, MONGO_CONFIG.port), undefined, { keep: database => database !== MONGO_CONFIG.db });
+            });
+
+            runTests(
+                path.join(__dirname, 'mongo_start_session.test.out'),
+                [{
+                    it: 'should inset start session record in chimera',
+                    args: ['pilotone', 'circuitone', MONGO_CONFIG.host, `${MONGO_CONFIG.port}`, MONGO_CONFIG.db, 'chimera'],
+
+                }, {
+                    it: 'should inset start session record in random',
+                    args: ['pilot_one', 'circuit_one', MONGO_CONFIG.host, `${MONGO_CONFIG.port}`, MONGO_CONFIG.db, 'random'],
+
+                }],
+                async (prop) => {
+                    const mongoConnection = await MongoClient.connect(getMongoUri(prop.args[2], +prop.args[3]), { useUnifiedTopology: true });
+                    const collection = mongoConnection.db(prop.args[4]).collection(prop.args[5]);
+                    let coll = await collection.aggregate().toArray();
+                    mongoConnection.close();
+                    assert(coll.length === 1);
+                    assert(coll[0].pilot === prop.args[0]);
+                    assert(coll[0].race === prop.args[1]);
+                }
+            );
+        });
+
+
+
+
+
     });
 }
--- test/partial/source/utils/misc.ts
+++ test/partial/source/utils/misc.ts
@@ -5,15 +5,19 @@ function runCTest(path: string, args: string[]): number {
     return +res;
 }
 
+export function getMongoUri(host: string, port: number) {
+    return `mongodb://${host}:${port}`
+}
+
 export function runTests(
     cTestPath: string,
-    testsProperties: { testDescription: string, args: string[], [key: string]: any }[],
-    callback: (properities: { testDescription: string, args: string[], result: number, [key: string]: any }) => Promise<void>,
+    testsProperties: { it: string, args: string[], [key: string]: any }[],
+    callback: (properities: { it: string, args: string[], result: number, [key: string]: any }) => Promise<void>,
 ) {
     for (const p of testsProperties) {
-        it(p.testDescription, async function() {
+        it(p.it, async function() {
             p.result = runCTest(cTestPath, p.args);
-            await callback(p as { testDescription: string, args: string[], result: number, [key: string]: any });
+            await callback(p as { it: string, args: string[], result: number, [key: string]: any });
         });
     }
 }
