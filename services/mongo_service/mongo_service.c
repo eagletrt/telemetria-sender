0b0c8fbab18edd60ffce23ada455bc9cd9de3c2b
euberdeveloper <euberdeveloper@gmail.com>
2021-05-04 14:16:27 +0200

    fix(mongo_service): fix timestamp to string
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- services/mongo_service/mongo_service.c
+++ services/mongo_service/mongo_service.c
@@ -7,17 +7,16 @@ static const char* MONGODB_APP_NAME = "E-agle racing team - Telemetria";
 static char* getStringPort(int port);
 static char* getUri(char* host, char* port);
 static mongodb_instance_t* getInstance(char* uri, char* db, char* collection);
-static char* itostr(int value);
 static char* getFormattedTimestamp(long long int timestamp);
 static char* getSessionName(const char* formatted_timestamp, const char* pilot, const char* race, const char* model_version);
 static bson_t* sessionDocumentBson(const char* formatted_timestamp, const char* pilot, const char* race, long long int timestamp, const char* session_name, const char* model_version);
 
 static char* getStringPort(int port) {
 	int length = digitsCount(port) + 1;
-	char *port_string = (char*) malloc(sizeof(char) * length);
+	char* port_string = (char*)malloc(sizeof(char) * length);
 
 	for (int i = 4; i >= 0; --i) {
-		port_string[i] = ((char) (port % 10)) + '0';
+		port_string[i] = ((char)(port % 10)) + '0';
 		port /= 10;
 	}
 	port_string[length - 1] = '\0';
@@ -27,19 +26,19 @@ static char* getStringPort(int port) {
 
 static char* getUri(char* host, char* port) {
 	int length = strlen(MONGODB_PREFIX) + strlen(host) + 1 + strlen(port) + 1;
-	char* uri = (char*) malloc(sizeof(char) * length);
+	char* uri = (char*)malloc(sizeof(char) * length);
 
 	strcpy(uri, MONGODB_PREFIX);
 	strcat(uri, host);
 	strcat(uri, ":");
 	strcat(uri, port);
-	
+
 	return uri;
 }
 
 static mongodb_instance_t* getInstance(char* uri, char* db, char* collection) {
-	mongodb_instance_t* instance = 
-		(mongodb_instance_t*) malloc(sizeof(mongodb_instance_t));
+	mongodb_instance_t* instance =
+		(mongodb_instance_t*)malloc(sizeof(mongodb_instance_t));
 
 	bson_error_t error;
 	instance->uri = mongoc_uri_new_with_error(uri, &error);
@@ -61,60 +60,20 @@ static mongodb_instance_t* getInstance(char* uri, char* db, char* collection) {
 	return instance;
 }
 
-static char* itostr(int value) {
-	char* toRtn = (char*) malloc(sizeof(char) * 15);
-	int reverted = 0;
-
-	int times = 0;
-	while (value != 0) {
-		reverted = (reverted*10) + value%10;
-		value /= 10;
-		times++;
-	}
-
-	int pos = 0;
-	while (times-- > 0) {
-		toRtn[pos++] = (reverted % 10) + '0';
-		reverted /= 10;
-	}
-
-	toRtn[pos] = 0;
-	return toRtn;
-}
-
 static char* getFormattedTimestamp(long long int timestamp) {
-	char* result;
+	const int length = 16;
+	char* result = (char*)malloc(length * sizeof(char));
 
 	time_t t = (timestamp / 1000);
-	struct tm *formatted = localtime(&t);
+	struct tm* formatted = localtime(&t);
 
-	int date = formatted->tm_mday + (formatted->tm_mon + 1) * 100 + (formatted->tm_year + 1900) * 10000;
-	char* dateString = itostr(date);
-
-	int time = (formatted->tm_sec) + (formatted->tm_min) * 100 + (formatted->tm_hour) * 10000;
-	char* timeString = itostr(time);
-
-	int length = strlen(dateString) + 1 + strlen(timeString) + 1;
-	if (formatted->tm_hour) {
-		length++;
-	}
-	result = (char*) malloc(sizeof(char) * length);
-	strcpy(result, dateString);
-	strcat(result, "_");
-	if (formatted->tm_hour) {
-		strcat(result, "0");
-	}
-	strcat(result, timeString);
-
-	free(dateString);
-	free(timeString);
-
+	strftime(result, length, "%Y%m%d_%H%M%S", formatted);
 	return result;
 }
 
 static char* getSessionName(const char* formatted_timestamp, const char* pilot, const char* race, const char* model_version) {
 	const int length = strlen(formatted_timestamp) + 1 + strlen(pilot) + 1 + strlen(race) + 2 + strlen(model_version) + 1;
-	char* session_name = (char*) calloc(length, sizeof(char));
+	char* session_name = (char*)calloc(length, sizeof(char));
 
 	strcpy(session_name, formatted_timestamp);
 	strcat(session_name, "_");
@@ -152,8 +111,7 @@ mongo_code mongoSetup() {
 	condition.mongodb.instance = getInstance(uri, db, collection);
 	if (condition.mongodb.instance == NULL) {
 		return MONGO_SETUP_ERROR;
-	}
-	else {
+	} else {
 		return MONGO_OK;
 	}
 }
@@ -166,42 +124,40 @@ mongo_code mongoStartSession() {
 	const char* model_version = condition.structure.model_version;
 	char* session_name = getSessionName(formatted_timestamp, pilot, race, model_version);
 	condition.mongodb.instance->session_name = session_name;
-	
+
 	bson_t* session_document = sessionDocumentBson(formatted_timestamp, pilot, race, timestamp, session_name, model_version);
 	return mongoInsert(session_document);
 }
 
-mongo_code mongoInsert(bson_t *data) {
+mongo_code mongoInsert(bson_t* data) {
 	bson_error_t error;
 	const int outcome = mongoc_collection_insert_one(condition.mongodb.instance->collection, data, NULL, NULL, &error);
-	
+
 	if (!outcome) {
-		printf ("Error in inserting in mongodb: %s\n", error.message);
+		printf("Error in inserting in mongodb: %s\n", error.message);
 		return MONGO_INSERT_ERROR;
-	}
-	else {
+	} else {
 		return MONGO_OK;
 	}
 }
 
 void mongoQuit() {
 	mongoc_collection_destroy(condition.mongodb.instance->collection);
-   	mongoc_database_destroy(condition.mongodb.instance->database);
-   	mongoc_client_destroy(condition.mongodb.instance->client);
-   	mongoc_uri_destroy(condition.mongodb.instance->uri);
-   	mongoc_cleanup();
+	mongoc_database_destroy(condition.mongodb.instance->database);
+	mongoc_client_destroy(condition.mongodb.instance->client);
+	mongoc_uri_destroy(condition.mongodb.instance->uri);
+	mongoc_cleanup();
 }
 
 char* mongoErrorMessage(mongo_code code) {
-    switch (code)
-    {
-        case MONGO_OK:
-            return strdup("MongoDB ok");
-        
-        case MONGO_SETUP_ERROR:
-            return strdup("Error in mongodb setup");
+	switch (code) {
+		case MONGO_OK:
+			return strdup("MongoDB ok");
 
-        default:
-            return strdup("Unknown mongodb error code");
-    }
+		case MONGO_SETUP_ERROR:
+			return strdup("Error in mongodb setup");
+
+		default:
+			return strdup("Unknown mongodb error code");
+	}
 }
